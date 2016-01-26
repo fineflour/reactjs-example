43a44eefa2afa9d48211f6457dbdb0d813904260
fineflour <soojin.cho@bfa.org>
2016-01-26 14:22:58 -0800

    finished pg23

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
-  root to: 'users#index' 
+  root to: 'meetups#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
